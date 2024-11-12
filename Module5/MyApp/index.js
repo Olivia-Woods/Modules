782cf01b8cd451377140f201a17c2df95139db3e
Olivia-Woods <oarwoods@gmail.com>
2024-11-12 21:20:25 +0800

    Updates

---
+++ Module5/MyApp/index.js
@@ -0,0 +1,13 @@
+const express = require('express'); // Importing the 'Express' library. This command loads the Express module from the project's dependencies.
+
+const app = express(); // Creating an instance of Express, which we use to set up and configure our web server.
+
+const port = 3000; // Defining the port number for our server to listen on, which will be http://localhost:3000.
+
+app.get(`/`, (req, res) => { // Setting up an endpoint. 'GET' is the HTTP method, used to request data from this route.
+    res.send('Hello World'); // Sending a response of 'Hello World' to the client when they access the root URL ('/').
+});
+
+app.listen(port, () => { // Starting the server to listen on the specified port.
+    console.log(`Server can now be called at http://localhost:${port}`); // Logging a message to the console to indicate that the server is running and accessible.
+});
