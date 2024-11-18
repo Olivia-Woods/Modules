d2be44c991402fa7ed6fd2de4091aeb58227f772
Olivia-Woods <oarwoods@gmail.com>
2024-11-18 11:08:29 +0800

    Updates

--- Module5/MyApp/index.js
+++ Module5/MyApp/index.js
@@ -1,10 +1,14 @@
 const express = require("express"); // Importing the 'Express' library. This command loads the Express module from the project's dependencies.
 
+const testRoutes = require("./Routes/myTestRoutes");
+
 const app = express(); // Creating an instance of Express, which we use to set up and configure our web server.
 
 const port = 3000; // Defining the port number for our server to listen on, which will be http://localhost:3000.
 
 app.use(`/`, express.static("Public"));
+
+app.use("/myOwnTest", testRoutes);
 
 app.get(`/test`, (req, res) => {
   // Setting up an endpoint. 'GET' is the HTTP method, used to request data from this route.
