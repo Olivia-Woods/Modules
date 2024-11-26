b8d7d7dfcf2ed7ff5e0d557ec27fbb54c73547bc
Olivia-Woods <oarwoods@gmail.com>
2024-11-26 19:05:58 +0800

    Updates

--- Module5/MyApp/controllers/calculatorController.js
+++ Module5/MyApp/controllers/calculatorController.js
@@ -1,18 +1,32 @@
+// controllers/calculatorController.js
+
+const calculator = require("../utils/calculator");
+const idGenerator = require("../utils/idGenerator");
+const logger = require("../utils/logger");
+
 function addNumbers(req, res) {
   const { num1, num2 } = req.query;
   const num1Num = parseInt(num1, 10);
   const num2Num = parseInt(num2, 10);
-  const result = num1Num + num2Num;
 
   // Validate Inputs
   if (Number.isNaN(num1Num) || Number.isNaN(num2Num)) {
     return res.status(400).json({
-      error: "The Inputs Are NOT Correct Numbers!",
+      error: "The Inputs are NOT Correct Numbers!",
     });
   }
 
+  // Use Calculator library
+  const result = calculator.add(num1Num, num2Num);
+
+  // Generate ID
+  const id = idGenerator.generateId();
+
+  // Log the call
+  logger.logMessage(id, result);
+
   // Send Result as JSON
-  res.json({ result });
+  res.json({ id, result });
 }
 
 module.exports = {
---
+++ Module5/MyApp/utils/logger.js
@@ -0,0 +1,7 @@
+const logMessage = (id, result) => {
+  console.log(`ID: ${id}, Result: ${result}`);
+};
+
+module.exports = {
+  logMessage,
+};
