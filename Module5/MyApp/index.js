0c811b5e66cb6874889f60364d7c90c2e6b9d1c7
Olivia-Woods <oarwoods@gmail.com>
2024-11-26 20:11:46 +0800

    Updates

--- Module5/MyApp/index.js
+++ Module5/MyApp/index.js
@@ -1,7 +1,19 @@
 const app = require("./app");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const port = 3000;
 
+// Integrating Swagger UI
+app.use(
+  "/api-docs", // Swagger UI will be accessible at http://localhost:3000/api-docs
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
+
 app.listen(port, () => {
   console.log(`Server can now be called at ${port}`);
+  console.log(
+    `API documentation available at http://localhost:${port}/api-docs`
+  );
 });
