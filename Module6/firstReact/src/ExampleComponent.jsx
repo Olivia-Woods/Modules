f8ee280db828e8ada8a68a6001a3000ba31a9d3a
Olivia-Woods <oarwoods@gmail.com>
2024-12-03 17:16:29 +0800

    Updates

---
+++ Module6/firstReact/src/ExampleComponent.jsx
@@ -0,0 +1,8 @@
+function ExampleComponent() {
+  return (
+    <div className="ExampleComponent componentBox">
+      <h1>My Example Component</h1>
+      <p>My First React Component!</p>
+    </div>
+  );
+}
