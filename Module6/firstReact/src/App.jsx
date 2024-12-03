2efe0a557423e4443c4787a84bb03117eff568f2
Olivia-Woods <oarwoods@gmail.com>
2024-12-03 19:16:25 +0800

    Updates

--- Module6/firstReact/src/App.jsx
+++ Module6/firstReact/src/App.jsx
@@ -9,16 +9,18 @@ function App() {
   const commentData = [
     {
       author: {
-        name: "John Doe",
-        avatarUrl: "https://via.placeholder.com/150",
+        name: "Olive Garden",
+        avatarUrl:
+          "https://www.thetutuguru.com.au/wp-content/uploads/2024/09/Olive-Tree-Topiary-1.jpg",
       },
       text: "This is a great example of a compositional React component!",
       date: new Date(),
     },
     {
       author: {
-        name: "Jane Smith",
-        avatarUrl: "https://via.placeholder.com/150",
+        name: "Olivia Woods",
+        avatarUrl:
+          "https://i.pinimg.com/736x/5b/6f/cb/5b6fcb70b51f09eeb0dd0a42d6eee546.jpg",
       },
       text: "I love how reusable these components are!",
       date: new Date(),
