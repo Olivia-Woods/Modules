7e7b04baaa9e978a75cb7ead5bceaf6b74ee030c
Olivia-Woods <oarwoods@gmail.com>
2025-01-09 16:47:40 +0800

    Updates

---
+++ Module7/LE/LE2.jsx
@@ -0,0 +1,4 @@
+// Update your BitcoinRates component to use a custom hook
+// for extracting the external data synchronization process
+// Extension: Implement useReducer to handle the internal
+// state management of your custom hook
